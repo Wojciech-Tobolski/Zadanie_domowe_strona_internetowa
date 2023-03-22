a083b4ef33f516d35d91ec2b4dc0790170a19ba1
Wojciech Tobolski <72449959+Wojciech-Tobolski@users.noreply.github.com>
2023-03-22 02:24:55 +0100

    jeszcze brakuje ciasteczek

--- scripts/ranking.js
+++ scripts/ranking.js
@@ -1,26 +1,56 @@
-const images = ["../images/Allain.jpg", "../images/The_lion_king.jpg"];
+setTimeout(Ready,1000);
 
-// Pobierz element HTML, do którego chcesz dodać obrazy
-const imageContainer = document.getElementById("image-container");
+function Ready() {
+  const images = ["../images/Alladin.jpg", "../images/The_lion_king.jpg", "../images/Lady_and_the_Tramp.jpg", "../images/Beauty_and_the_Beast.jpg"];
 
-// Przejdź przez listę obrazów i utwórz element <img> dla każdego z nich
-for (let i = 0; i < images.length; i++) {
-  const image = document.createElement("img");
-  image.src = images[i];
+  const imageContainer = document.getElementById("image-container");
 
-  // Dodaj element <img> do elementu HTML
-  imageContainer.appendChild(image);
-}
-const firstImage = document.querySelector("#image-container img:first-child");
+  let startIndex = 0;
+  let endIndex = 1;
 
+  function loadNextImages() {
+    startIndex += 2;
+    endIndex += 2;
 
-  function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+    if (endIndex < images.length) {
+      for (let i = startIndex; i <= endIndex; i++) {
+        const image = document.createElement("img");
+        image.src = images[i];
+        image.classList.add("image-item"); // Dodaj klasę do obrazu
+        imageContainer.appendChild(image);
+      }
+    } else {
+      for (let i = startIndex; i < images.length; i++) {
+        const image = document.createElement("img");
+        image.src = images[i];
+        image.classList.add("image-item"); // Dodaj klasę do obrazu
+        imageContainer.appendChild(image);
+      }
     }
-    return color;
   }
-  
-  setInterval(changeBackground, 5000);
+
+  for (let i = startIndex; i <= endIndex; i++) {
+    const image = document.createElement("img");
+    image.src = images[i];
+    image.classList.add("image-item"); // Dodaj klasę do obrazu
+    imageContainer.appendChild(image);
+  }
+
+  // Znajdź obrazy z klasą "image-item" i dodaj nasłuchiwanie na kliknięcie
+  const imagesList = document.querySelectorAll(".image-item");
+  for (let i = 0; i < imagesList.length; i++) {
+    imagesList[i].addEventListener("click", function() {
+      // Usuń wszystkie obrazy z klasą "image-item"
+      console.log(`Kliknięto obraz ${i}`);
+      const imagesToRemove = document.querySelectorAll(".image-item");
+      for (let j = 0; j < imagesToRemove.length; j++) {
+        imagesToRemove[j].remove();
+      }
+
+      // Sprawdź, czy należy wczytać kolejne obrazy
+      if (imagesList.length <= 2 && images.length > 0) {
+        loadNextImages();
+      }
+    });
+  }
+}
